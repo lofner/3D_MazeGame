eefabb61d22a55ccb834fd4f53d39fa7ff783a14
lofner <lofner4@gmail.com>
2020-06-09 22:54:33 +0200

    ScoreManger with CoinScore; CoinAnimation

---
+++ 3D_MazeGame/Assets/Scenes/Game/ScoresManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoresManager : MonoBehaviour
+{
+    public Text CoinsDisplay;
+    public static ScoresManager instance;
+    public int CoinsScore;
+
+    void Start()
+    {
+        instance = this;
+
+        //set all Scores
+        CoinsScore = 0;
+    }
+
+    void Update()
+    {
+        CoinsDisplay.text = CoinsScore.ToString();
+    }
+}
